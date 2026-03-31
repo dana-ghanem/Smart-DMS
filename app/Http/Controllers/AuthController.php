45b807c2b9955ce89ee21ad0ad596d62c8411b25
Hanan Fatayri <alfatayrihanan@gmail.com>
2026-03-31 13:25:54 +0300

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
             $request->session()->regenerate();
-            return redirect()->intended(route('documents.index'));
+            return redirect()->route('documents.index');
         }
 
         return back()->withErrors([
